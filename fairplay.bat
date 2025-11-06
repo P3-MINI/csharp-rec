2895f8246dd7d3cf12bff68e634052ceda5128f9
Tomasz Herman <tomasz.herman@protonmail.com>
2025-11-06 16:39:50 +0100

    Initial commit

---
+++ fairplay.bat
@@ -0,0 +1,3 @@
+net use Z: "\\nonus\teachers"
+cd /D Z:\hermant\public\sop-rec-windows
+p3-rec.exe
---
+++ p3-rec/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+string user = Environment.UserName;
+
+string recordingPath;
+string[] ffmpegArgs;
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+{
+    recordingPath = $"/home2/samba/spychalam/public/recordings/{GetFileName(user)}";
+    ffmpegArgs = ["-framerate", "1", "-f", "x11grab", "-i", ":0.0", "-c:v", "libx264", "-g", "10", "-keyint_min", "10", "-preset", "fast", "-f", "mpegts", recordingPath];
+}
+else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+{
+    recordingPath = $"Z:\\spychalam\\public\\recordings\\{GetFileName(user)}";
+    ffmpegArgs = ["-framerate", "1", "-f", "gdigrab", "-i", "desktop", "-c:v", "libx264", "-g", "10", "-keyint_min", "10", "-preset", "fast", "-f", "mpegts", recordingPath];
+}
+else
+{
+    throw new PlatformNotSupportedException("Unknown OS, not supported");
+}
+
+Process ffmpeg = Process.Start("ffmpeg", ffmpegArgs);
+ffmpeg.WaitForExit();
+
+string GetFileName(string username, string course = "P3")
+{
+    var url = $"https://ghlabs.mini.pw.edu.pl/create_recordings?username={username}&course={course}";
+    using var client = new HttpClient();
+    return client.GetStringAsync(url).Result;
+}
