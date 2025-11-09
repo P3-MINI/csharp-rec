e2ec4558f0a8aa0c045b78628e3441d099a42c79
Tomasz Herman <tomasz.herman@protonmail.com>
2025-11-09 22:45:23 +0100

    Added recording in progress text

--- p3-rec/Program.cs
+++ p3-rec/Program.cs
@@ -1,27 +1,55 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
+using Spectre.Console;
 
-string user = Environment.UserName;
+var recordingMessage = new FigletText("Recording in progress")
+    .Centered()
+    .Color(Color.Green);
 
-string recordingPath;
-string[] ffmpegArgs;
-if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-{
-    recordingPath = $"/home2/samba/spychalam/recordings/{GetFileName(user)}";
-    ffmpegArgs = ["-framerate", "1", "-f", "x11grab", "-i", ":0.0", "-c:v", "libx264", "-g", "10", "-keyint_min", "10", "-preset", "fast", "-f", "mpegts", "-y", "-loglevel", "quiet", recordingPath];
-}
-else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-{
-    recordingPath = $"Z:\\spychalam\\recordings\\{GetFileName(user)}";
-    ffmpegArgs = ["-framerate", "1", "-f", "gdigrab", "-i", "desktop", "-c:v", "libx264", "-g", "10", "-keyint_min", "10", "-preset", "fast", "-f", "mpegts", "-y", "-loglevel", "quiet", recordingPath];
-}
-else
-{
-    throw new PlatformNotSupportedException("Unknown OS, not supported");
-}
+var ffmpeg = StartFFmpeg();
 
-Process ffmpeg = Process.Start("ffmpeg", ffmpegArgs);
-ffmpeg.WaitForExit();
+AnsiConsole.Live(recordingMessage)
+    .Start(ctx =>
+    {
+        Console.CancelKeyPress += (_, e) =>
+        {
+            AnsiConsole.MarkupLine("[bold red]Stopping recording...[/]");
+            ffmpeg.Kill(true);
+            e.Cancel = true;
+        };
+
+        while (!ffmpeg.HasExited)
+        {
+            ctx.Refresh();
+            Thread.Sleep(500);
+        }
+    });
+
+AnsiConsole.MarkupLine("[bold green]Recording finished![/]");
+
+Process StartFFmpeg()
+{
+    string user = Environment.UserName;
+    string recordingPath;
+    string[] ffmpegArgs;
+
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+    {
+        recordingPath = $"/home2/samba/spychalam/recordings/{GetFileName(user)}";
+        ffmpegArgs = ["-framerate", "1", "-f", "x11grab", "-i", ":0.0", "-c:v", "libx264", "-g", "10", "-keyint_min", "10", "-preset", "fast", "-f", "mpegts", "-y", "-loglevel", "quiet", recordingPath];
+    }
+    else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    {
+        recordingPath = $"Z:\\spychalam\\recordings\\{GetFileName(user)}";
+        ffmpegArgs = ["-framerate", "1", "-f", "gdigrab", "-i", "desktop", "-c:v", "libx264", "-g", "10", "-keyint_min", "10", "-preset", "fast", "-f", "mpegts", "-y", "-loglevel", "quiet", recordingPath];
+    }
+    else
+    {
+        throw new PlatformNotSupportedException("Unknown OS, not supported");
+    }
+
+    return Process.Start("ffmpeg", ffmpegArgs) ?? throw new Exception("Failed to start ffmpeg");
+}
 
 string GetFileName(string username, string course = "P3")
 {
